24f35316a3a6813c39790a446176daddbc4623c7
Gravituss <zaretskyyy@gmail.com>
2016-04-12 23:31:02 +0300

    preparing data for input

--- CommonMethods.java
+++ CommonMethods.java
@@ -25,7 +25,7 @@ public class CommonMethods {
 	public static final String ID_PASSWORD_TEXT_AREA = "login_password";
 	public static final String LOCATOR_ENTER_BUTTON = "//*[contains(@class,'enter-submit')]";
 	
-	//----------------------------------------------------------
+	//--------------------------- Constants for more test cases
 	
 	public static final String ID_EMAIL_TEXT_AREA_REG = "register_username";
 	public static final String ID_NAME_TEXT_AREA_REG = "register_name";
@@ -33,16 +33,28 @@ public class CommonMethods {
 	public static final String ID_NICKNAME_TEXT_AREA_REG = "register_nickname";
 	public static final String ID_PASSWD_TEXT_AREA_REG = "register_password";
 	public static final String ID_PASSWD_AGAIN_TEXT_AREA_REG = "register_password_confirm";
-	
+	//-----
 	public static final String ID_DAY_OF_BIRTH_REG = "register_b_day";
-	public static final String ID_MONTH_OF_BIRTH_REG =  "register_b_month";
-	public static final String ID_YEAR_OF_BIRTH_REG =  "register_b_year";
+	public static final String ID_MONTH_OF_BIRTH_REG = "register_b_month";
+	public static final String ID_YEAR_OF_BIRTH_REG = "register_b_year";
+	//-----
+	public static final String ID_AGREED_CHECKBOX_REG = "register_agreed";
+	public static final String LOCATOR_BUTTON_REG = "//*[@role='button'][@onclick='register()']";
 	
-	public static final String ID_AGREED_CHECKBOX_REG =  "register_agreed";
-	public static final String LOCATOR_BUTTON_REG =  "//*[@role='button'][@onclick='register()']";
+	//--------------------------- Constants for input
 	
-	//----------------------------------------------------------
+	public static final String IN_EMAIL_TEXT_AREA_REG = "dljfldjfldkss53@gmail.com";
+	public static final String IN_NAME_TEXT_AREA_REG = "Harry";
+	public static final String IN_SURNAME_TEXT_AREA_REG = "Bitkinson";
+	public static final String IN_NICKNAME_TEXT_AREA_REG = "Ladlkfdjlfk6546";
+	public static final String IN_PASSWD_TEXT_AREA_REG = "kjhkuh65";
+	public static final String IN_PASSWD_AGAIN_TEXT_AREA_REG = "lrdss6846";
+	//-----
+	public static final String IN_DAY_OF_BIRTH_REG = "08";
+	public static final String IN_MONTH_OF_BIRTH_REG = "декабря";
+	public static final String IN_YEAR_OF_BIRTH_REG = "1987";
 	
+	//--------------------------------------------
 	
 	static WebDriver driver = new FirefoxDriver();
 	
