995c4021764735ba098d3321cde212350dc33320
Gravituss <zaretskyyy@gmail.com>
2016-04-12 12:59:17 +0300

    adding TCase07

---
+++ TCase07PasswdTextArea.java
@@ -0,0 +1,30 @@
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+
+public class TCase07PasswdTextArea {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		CommonMethods.driver.quit();
+	}
+
+	@Test
+	public void test() {
+		
+		CommonMethods.openPageSelectRussianAndClickRegister();
+		
+		WebElement pwdTextArea = CommonMethods.driver.findElement(By.id("login_password"));
+		boolean enabled = pwdTextArea.isEnabled();
+		assertTrue(enabled);			
+	}
+}
